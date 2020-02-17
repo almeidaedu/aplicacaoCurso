63583088132d206df13c9e21f39494d052ff985d
eduAlmeida1 <eduardo.almeida@cadmus.com.br>
2020-02-16 21:02:24 -0300

    Notificando novos agendamentos com MongoDB

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,8 +1,10 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 import User from '../models/User';
 import File from '../models/File';
 import Appointment from '../models/Appointment';
+import Notification from '../schemas/Notification';
 
 class AppointmentController {
   async index(req, res) {
@@ -61,7 +63,6 @@ class AppointmentController {
     /**
      * Check for past dates
      */
-
     const hourStart = startOfHour(parseISO(date));
 
     if (isBefore(hourStart, new Date())) {
@@ -71,7 +72,6 @@ class AppointmentController {
     /**
      * Check date availability
      */
-
     const checkAvailability = await Appointment.findOne({
       where: {
         provider_id,
@@ -90,6 +90,21 @@ class AppointmentController {
       user_id: req.userId,
       provider_id,
       date,
+    });
+
+    /**
+     *  Notify appointment Provider
+     */
+    const user = await User.findByPk(req.userId);
+    const formattedDate = format(
+      hourStart,
+      "'dia' dd 'de' MMMM', às' H:mm'h'",
+      { locale: pt }
+    );
+
+    await Notification.create({
+      content: `Novo agendamento de ${user.name} para ${formattedDate}`,
+      user: provider_id,
     });
 
     return res.json(appointment);
