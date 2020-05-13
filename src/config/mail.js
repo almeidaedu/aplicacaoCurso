96f11f1c8f42b1b727ea6d0f1b5951e2bdc2537e
eduAlmeida1 <eduardo.almeida@cadmus.com.br>
2020-05-12 21:06:03 -0300

    envio de email completo

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '0a2cab72fb0d9b',
+    pass: 'ef594a85776ef9',
+  },
+  default: {
+    from: 'Eduardo Almeida <noreply@gorbarber.com>',
+  },
+};
